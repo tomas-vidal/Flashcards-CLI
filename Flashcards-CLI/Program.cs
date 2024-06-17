1bbbf1d23bdeffb22984b778644727c39139b952
Tomas <vidalltomas@gmail.com>
2024-06-17 12:59:08 -0300

    Added main menu

--- Flashcards-CLI/Program.cs
+++ Flashcards-CLI/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+
+public static class Program
+{
+    public static bool closeApp = false;
+    public static void Main(string[] args)
+    {
+        while(!closeApp)
+        {
+            AnsiConsole.Write(new FigletText("Main menu").Color(Color.Green));
+            AnsiConsole.Markup("\t[red]0 - Type 0 to close the app[/]\n");
+            AnsiConsole.Markup("\t[blue]1 - Type 1 to manage stacks[/]\n");
+            AnsiConsole.Markup("\t[yellow]2 - Type 2 to start a study session[/]\n");
+            AnsiConsole.Markup("\t[pink1]3 - Type 3 view all study sessions[/]\n");
+        }
+    }
+}
